a5620d7e1a6b3741775a5c515eabe1978651852d
たかえすみずき <takaesumizuki@takaesumizuki-no-MacBook-Air.local>
2017-04-09 18:38:21 +0900

    タイトルから本を検索できるように

--- library/Library.java
+++ library/Library.java
@@ -1,18 +1,39 @@
 import java.util.*;
 
 public class Library{
+    List<Book> shelf = new ArrayList<Book>();
+
     void run(){
         this.printWelcomeMessage();
-        List<Book> shelf = new ArrayList<Book>();
         this.addBooks(shelf);
+        this.list();
+        this.printBook(this.find("聖教新聞"));
+        this.printBook(this.find("腹腹時計"));
+    }
 
-        for(Integer i = 0; i < shelf.size(); i++){
-            Book book = shelf.get(i);
+    Book find(String searchName){
+        Book foundBook = null;
+        for(Book book: shelf){
+            if(Objects.equals(book.title,searchName)){
+                foundBook = book;
+            }
+        }
+        return foundBook;
+    }
+
+    //本の一覧を表示するメソッド
+    void list(){
+        for(Book book: shelf){
             this.printBook(book);
         }
     }
 
+    // 一冊の本の情報を表示するメソッド
     void printBook(Book book){
+        if(book == null){
+            System.out.println("そんな本ないです");
+            return;
+        }
         System.out.printf("%s (%s) %s, %d%n",
         book.title, book.authors, book.publisher, book.publishYear);
     }
