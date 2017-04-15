741833e7e27d19e019199395723f185e09389371
たかえすみずき <takaesumizuki@takaesumizuki-no-MacBook-Air.local>
2017-04-15 10:53:20 +0900

    説明書きを追加(SouceTreeのテスト)

--- library/History.java
+++ library/History.java
@@ -9,6 +9,7 @@ public class History{
         else return false;
     }
 
+    //日付を表示するメソッドです
     void print(){
         if(this.isLent()){
             System.out.print(this.book.title + ", " + this.book.authors + ", " + this.book.publisher +", "+ this.book.publishYear + " (貸出中), " + user.name + "("  + user.gender + "; " + user.age + ") ");
